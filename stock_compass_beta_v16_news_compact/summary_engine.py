dd6ae48453dbe312825705eed80ecc7dd7401724
agseen77-oss <agseen77@gmail.com>
2026-06-18 15:31:10 +0900

    V16 compact news update

---
+++ stock_compass_beta_v16_news_compact/summary_engine.py
@@ -0,0 +1,8 @@
+def get_today_summary():
+    return {
+        "market_status": "강세",
+        "main_theme": "전력",
+        "main_stock": "대한전선",
+        "strategy": "관심 유지, 무리한 추격매수는 주의",
+        "comment": "오늘은 전력 인프라 테마가 가장 강하게 부각되고 있으며, 대한전선을 대표 후보로 볼 수 있습니다."
+    }
